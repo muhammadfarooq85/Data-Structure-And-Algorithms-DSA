566295d3fb8fc74a9b8a8a57b214d38f1815ed13
Muhammad Farooq <irfankhans786787788@gmail.com>
2025-02-15 21:45:21 +0500

    tree data strcuture and its types

---
+++ Ashih-Saluja/Tree-Data-Structure/Tree-Data-Structure.js
@@ -0,0 +1,41 @@
+// how does file system stores its data
+// every folders have so many files and folders
+// and then that folder or file have many folders and files
+// this creates a tree like structure
+// where ther parent children data structure you can use Tree data structure
+
+// Tree data structure is a non-linear data structure
+// stack, qeue, are linear data structure
+
+// every node has parent except root element
+// each subtree of node is itself a tree
+
+// types of trees
+
+// binary tree
+// each node has exactly two children (left and right)
+
+// binary search tree
+// each node child has also eactly two nodes
+// also left child < parent
+// also right > parent
+
+// avl tree
+// self balancing search tree
+// diference between the height of left and right subtree of any node must be between 1 and -1.
+
+// trie tree
+// special tree for storing strings
+// useful for autocomplete, spell checking and prefix based searching
+
+// N-ary tree
+// node have more than two childrens
+// example like file system etc.
+
+// where to use tree data structure
+
+// file system
+// networking
+// DOM
+// databases
+
