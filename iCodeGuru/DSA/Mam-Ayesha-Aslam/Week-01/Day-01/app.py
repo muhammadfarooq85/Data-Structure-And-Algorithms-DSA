81928b35142bd91a91eb6def69f2db81ad52689e
Muhammad Farooq <irfankhans786787788@gmail.com>
2025-02-27 19:40:11 +0500

    Day-01

---
+++ iCodeGuru/DSA/Mam-Ayesha-Aslam/Week-01/Day-01/app.py
@@ -0,0 +1,46 @@
+# data
+## data is a collection of raw data
+
+# structure
+## it is a way of organizing
+
+# Algorithms
+## Aa step by step procedure for sloving a problem
+
+# DSA
+## these are actually an essential way to organize and store data efficently in computer
+
+# Why to learn DSA?
+## to acheive problem solving skills
+## Building scalable solution in web and app
+## interview preparation
+
+# Types of data
+
+## primitve data types
+## non -primitve data types
+### primitive data types" refer to basic, built-in data types like integers, characters, and booleans that directly store their values in memory, while "non-primitive data types" are more complex structures like arrays, objects, and strings which store a reference to the data's location in memory, allowing for more complex manipulation and mutability
+
+
+# Most common data structure
+## Array 
+## hashmaps/key value pairs
+## lnkedlist
+## stack
+## qeue
+## tree graphs
+
+# Array DSA
+## array is linear data structure. It is a collection of different types of data
+
+# Why we use store arrays DSA?
+## due to store multiple data in the single array
+
+# How to access elements in an array?
+## Every element of an array can accessed through its index. Index of array always starts with 0 and ends at N-1.
+
+# Types of Arrays
+## Arrays can be categorized into different types:
+## One dimensional array
+## Two dimensional array
+## Three dimensional array
