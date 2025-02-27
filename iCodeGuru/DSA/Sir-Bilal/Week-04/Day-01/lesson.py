64437ea59e82017a46697737a256cc3b89445551
Muhammad Farooq <irfankhans786787788@gmail.com>
2025-02-27 12:58:37 +0500

    Day-01

---
+++ iCodeGuru/DSA/Sir-Bilal/Week-04/Day-01/lesson.py
@@ -0,0 +1,18 @@
+# Two pointers
+
+# fundamental approach to slove a problem efficiently
+# it uses pairs of indices or pointers ( left and right)
+# example use case such as
+# in finding the pair in array who as sum closes to x
+# searching pairs in sorted array
+
+
+# types of two pointer
+# some time in same direction and some time in opposite direction
+# slow moving pointer
+# fast moving pointer
+
+# leetcode problem
+# 26:- Remove duplicates from sorted array
+
+# successfully solved two leetcode problems. 60% persent understand hoe question
