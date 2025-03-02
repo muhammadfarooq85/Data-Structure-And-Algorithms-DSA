be969028680135e284efc6e3e07dbe272295493c
Muhammad Farooq <irfankhans786787788@gmail.com>
2025-03-02 12:00:24 +0500

    Day-02

--- iCodeGuru/DSA/Mam-Ayesha-Aslam/Week-01/Day-02/app.py
+++ iCodeGuru/DSA/Mam-Ayesha-Aslam/Week-01/Day-02/app.py
@@ -8,3 +8,17 @@ class Solution:
         return ans + nums
     
 print(Solution.concatenateArray([1,2,3,5]))
+
+# 1295. Find Numbers with Even Number of Digits
+
+class Solution1:
+    def findNumbers(nums):
+        count = 0
+        for num in nums:
+            strNum = str(num)
+            if len(strNum) % 2 == 0:
+                count += 1
+                
+        return count
+    
+print(Solution1.findNumbers([12,345,2,6,7896])) # 2
