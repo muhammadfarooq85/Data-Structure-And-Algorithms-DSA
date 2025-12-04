82c55e93a04df2edc47a10df8591d93f13bde747
Muhammad Farooq <enmuhammadfarooq@gmail.com>
2025-12-04 21:20:11 +0500

    Day-18

---
+++ Love-Babbar-CPP/Day-18/time-space.cpp
@@ -0,0 +1,5 @@
+// Time and Space Complexity
+
+// Time complexity measures how the execution time of an algorithm changes with the size of the input. How fast or slow an algorithm runs as the input grows.
+
+// Space complexity measures how much memory (space) an algorithm needs relative to the input size. How much extra storage the algorithm requires to run.
